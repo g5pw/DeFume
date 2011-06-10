574d762f5ffc29206dfe5ffb357c8a5b69ba9473
Aljaž Srebrnič <a2piratesoft@gmail.com>
2011-06-10 17:00:09 +0200

    Deleted unused files
    Signed-off-by: Aljaž Srebrnič <a2piratesoft@gmail.com>

--- FanCtrl.X/build/default/production/main.p1.d
+++
@@ -1,5 +0,0 @@
-build/default/production/main.p1:\
- /Applications/microchip/PICC-pro/include/htc.h\
- /Applications/microchip/PICC-pro/include/eeprom_routines.h\
- /Applications/microchip/PICC-pro/include/pic12f6x.h\
- /Applications/microchip/PICC-pro/include/pic.h\
