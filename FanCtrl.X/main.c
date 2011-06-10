f663d9836bd02ab2b3a85d972b3999e22570c398
Aljaž Srebrnič <a2piratesoft@gmail.com>
2011-06-10 17:16:06 +0200

    Simplified PWM controls
    Signed-off-by: Aljaž Srebrnič <a2piratesoft@gmail.com>

--- FanCtrl.X/main.c
+++ FanCtrl.X/main.c
@@ -8,20 +8,23 @@
 
 #include <pic.h>	// Required to interface with delay routines
 
+#define _XTAL_FREQ 32000000
+
 //Setting configuration bits
 __CONFIG(FOSC_INTOSC & WDTE_OFF & PWRTE_ON & MCLRE_OFF); //Config word 1
 __CONFIG(PLLEN_ON);
 
 
 int main(void) {
+    int time_on = 0;
     OSCCON = 0b11110000;    // Configure 32 MHz clock
 
     CCP1SEL = 1;            //CCP1 on RA5;
     TRISA = 0x3F;           // Everything is input (tri/stated)
     PR2 = 0xFF;             // Select 31250Hz PWM period
     CCP1CON = 0x0C;         // Select PWM single output
-    CCP1CON |= 0x30;        //Setting duty cycle: LSBits
-    CCPR1L = 0x0;           //Setting duty cycle: MSByte
+    CCP1CON |= (time_on & 3) << 4;
+    CCPR1L = time_on >> 2;           //Setting duty cycle: MSByte
     TMR2IF = 0;             // Tlear Timer 2 interrupt flag
     T2CON = 0x04;           // TMR2 Prescaler:1, Enable timer 2
     while(TMR2IF = 0);      // Wait until timer2=PS2 (TMR2IF=1)
