18eb683333c70098fc94335b3cd38bdc7f19849e
Aljaž Srebrnič <a2piratesoft@gmail.com>
2011-07-03 16:03:44 +0200

    PWM button controls now working
    Signed-off-by: Aljaž Srebrnič <a2piratesoft@gmail.com>

--- FanCtrl.X/main.c
+++ FanCtrl.X/main.c
@@ -10,9 +10,9 @@
 
 //Setting configuration bits
 __CONFIG(FOSC_INTOSC & WDTE_OFF & PWRTE_ON & MCLRE_OFF); //Config word 1
-__CONFIG(PLLEN_ON);
+__CONFIG(PLLEN_ON & LVP_OFF);
 
-volatile char oldPORTA = 0x3F;
+volatile char oldPORTA = 0x0F;
 
 void pwm_init(unsigned char period){
     CCP1SEL = 1;            //CCP1 on RA5;
@@ -40,36 +40,37 @@ int main(void) {
     OSCCON = 0xF0;    // Configure 32 MHz clock
     
     TRISA = 0x0F;           // Everything is an input
+    ANSELA = 0x00;
     nWPUEN = 0;
     WPUA = 0x0F;            // Pullup resistors on all pins except RA5
     IOCAN = 0x0F;           // Enable interrupts on change
     IOCIE = 1;
-    ei();
+    GIE = 1;
 
     pwm_init(0xFF);
     pwm_start(512);
+    RA4=1;
 
     while (1) { // Main loop
+        
     }
 }
 
 interrupt isr(){
-    if(IOCIF){ // Someone pressed on a button!
-        RA4 = 1;
-        __delay_ms(20); // Debounce
-        char temp = PORTA^oldPORTA;
-        oldPORTA = PORTA;
-        if(temp | 0x01){
-            TRISA &= ~(TRISA | 0x20); // Toggle PWM on/off
+    if(IOCIF){
+        if(IOCAF0){
+            TRISA5 = ~TRISA5;
+            IOCAF0 = 0;
         }
-        if(temp | 0x02){
-            CCPR1L += 4;        // increase PWM duty
+        if(IOCAF1){
+            CCPR1L += 4;
+            IOCAF1 = 0;
         }
-        if(temp | 0x04){
-            CCPR1L -= 4;        // Decrease PWM duty
+        if(IOCAF2){
+            CCPR1L -= 4;
+            IOCAF2 = 0;
         }
-        if(temp | 0x08){
-            //RA3 pressed
-        }
+        __delay_ms(10);
+        IOCIF = 0;
     }
 }
